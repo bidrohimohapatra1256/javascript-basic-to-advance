e881ba213217f544d2940f3a49bf64a9edc81ec5
Biderohi <biderohimohapatra@gmail.com>
2024-03-14 20:03:59 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+//  a java script varisble is a simple a name of stroage location
+// let usear->locatrion = ("bidrohi")<-value
+// let userName = ("bidrohi")
+
+// let secondName = ("my is bidrohi")
+// let age = 20
+//  userHappy =true
+//  let userName = ("bidu")
+//  let mon = userName + 'mohapatra' + ` ` + secondName +"i am" +age +'year old'
 
 
 
 
+//constant
 
-console.log("hello worls") 
+
+
+// code execution
+
+console.log( typeof (firstName))
+
+var firstName ="bidrohi"
+let lastName ="mohapatra"
+let age =15
+const yearOfBirth =1999
+
+
