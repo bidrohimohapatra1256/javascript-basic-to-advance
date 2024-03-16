d374172693bee6c4d76a6d1dbb9a8976df05ba76
Biderohi <biderohimohapatra@gmail.com>
2024-03-16 11:44:59 +0530

    today reading dilog box

---
+++ 1scrpit.js
@@ -0,0 +1,32 @@
+//  a java script varisble is a simple a name of stroage location
+// let usear->locatrion = ("bidrohi")<-value
+// let userName = ("bidrohi")
+
+//  let secondName = ("my is bidrohi")
+// let age = 20
+//  userHappy =true
+  // let userName = ("bidu")
+//  let mon = userName + 'mohapatra' + ` ` + secondName +"i am" +age +'year old'
+
+
+
+
+//constant
+
+
+
+// code execution
+
+// console.log( typeof ( typeof firstName))
+
+// var firstName ="bidrohi"
+// let lastName ="mohapatra"
+// let age =15
+// const yearOfBirth =1999
+
+//dilogbox
+// alert("are you sure.")
+// confirm("your massgae gors hear.")
+ const userInput = prompt("your massgae gors hear.")
+ console.log (userInput)
+
