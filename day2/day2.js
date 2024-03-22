21d1fc772825d32f99daa5d9039842f07953bcbe
Biderohi <biderohimohapatra@gmail.com>
2024-03-22 22:40:12 +0530

    com

--- day2/day2.js
+++ day2/day2.js
@@ -2,3 +2,4 @@
 //  methodes (with out arguments)
 const capitalmassge ='          BIDROHIMOHAPATRA'.trim()
 const cheakTheMassge='hii a am bidrohi'.includes()
+const cheakTheIndexNumber='hii a am bidrohi'.indexOf()
