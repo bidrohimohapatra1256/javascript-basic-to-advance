c86ec3e09abf784969657740d643c6b290d56f6f
Biderohi <biderohimohapatra@gmail.com>
2024-03-25 00:10:46 +0530

    commit

--- day2/day2.js
+++ day2/day2.js
@@ -4,3 +4,10 @@ const capitalmassge ='          BIDROHIMOHAPATRA'.trim()
 const cheakTheMassge='hii a am bidrohi'.includes()
 // cheak the indexnumber
 const cheakTheIndexNumber='hii a am bidrohi'
+// 
+const acconutId =216
+ let acconutEmail = "b1256@gmail.com"
+ var acconutPossword "917"
+ acconutCity ="bbbsr"
+ acconutId=2
+  console.log(acconutId);
