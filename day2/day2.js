41011f4fb0a8531e7b62cc093bc85dfe01e90232
Biderohi <biderohimohapatra@gmail.com>
2024-03-25 22:56:31 +0530

    commit

--- day2/day2.js
+++ day2/day2.js
@@ -5,9 +5,9 @@ const cheakTheMassge='hii a am bidrohi'.includes()
 // cheak the indexnumber
 const cheakTheIndexNumber='hii a am bidrohi'
 // 
-const acconutId =216
+const a =216
  let acconutEmail = "b1256@gmail.com"
- var acconutPossword "917"
+ var acconutPossword =917
  acconutCity ="bbbsr"
  acconutId=2
-  console.log(acconutId);
+  console.log(acconutId);cconutId
