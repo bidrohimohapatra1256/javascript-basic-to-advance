8fb6cc75a73539b59591813bf1285727df997134
Biderohi <biderohimohapatra@gmail.com>
2024-03-30 22:32:42 +0530

    commit

--- day1/1scrpit.js
+++ day1/1scrpit.js
@@ -66,4 +66,15 @@ function changeBackground(number) {
     backLayer.style.clipPath = "";
   });
   
+// NavBar Link Active
+let links = document.querySelectorAll('.nav-link');
+for(let i=0; i<links.length; i++){
+links[i].addEventListener('click', function() {
+    for(let j=0; j<links.length; j++)
+    links[j].classList.remove('active');
+    this.classList.add('active');
+});
+}
+
+  
 }
