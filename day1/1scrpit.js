7669d4a1ecce1e5c7734d709d4d3ec83fd86d309
Biderohi <biderohimohapatra@gmail.com>
2024-04-01 22:19:48 +0530

    commit

--- day1/1scrpit.js
+++ day1/1scrpit.js
@@ -54,18 +54,8 @@ function changeBackground(number) {
   const signupBtn = document.querySelector(".signup button");
   const backLayer = document.querySelector(".back-layer");
   
-  signupBtn.addEventListener("click", () => {
-    loginForm.classList.remove("active");
-    signupForm.classList.add("active");
-    backLayer.style.clipPath = "inset(0 0 0 50%)";
-  });
-  
-  loginBtn.addEventListener("click", () => {
-    signupForm.classList.remove("active");
-    loginForm.classList.add("active");
-    backLayer.style.clipPath = "";
-  });
-  
+  acconutId=2
+
 
 
   
