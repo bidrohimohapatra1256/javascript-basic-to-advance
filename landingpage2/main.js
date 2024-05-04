ae803f077950a567f53fabded8688245ca5901b5
Biderohi <biderohimohapatra@gmail.com>
2024-05-04 18:41:57 +0530

    commit

--- landingpage2/main.js
+++ landingpage2/main.js
@@ -3,10 +3,6 @@ function checkPalindrome() {
     const inputText = document.getElementById('inputText').value.toLowerCase().replace(/[^a-z0-9]/g, '');
     const reversedText = inputText.split('').reverse().join('');
     
-    if (inputText === reversedText) {
-        document.getElementById('result').innerText = `"${inputText}" is a palindrome.`;
-    } else {
-        document.getElementById('result').innerText = `"${inputText}" is not a palindrome.`;
-    }
+    
 }
 
