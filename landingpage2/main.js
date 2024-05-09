93e74611c98c7793b463461f3566d5a9bb5688ce
Biderohi <biderohimohapatra@gmail.com>
2024-05-09 23:44:07 +0530

    commit

--- landingpage2/main.js
+++ landingpage2/main.js
@@ -32,20 +32,6 @@ function highlightCups(idx) {
     updateBigCup()
 }
 
-function updateBigCup() {
-    const fullCups = document.querySelectorAll('.cup-small.full').length
-    const totalCups = smallCups.length
-
-    if(fullCups === 0) {
-        percentage.style.visibility = 'hidden'
-        percentage.style.height = 0
-    } else {
-        percentage.style.visibility = 'visible'
-        percentage.style.height = `${fullCups / totalCups * 330}px`
-        percentage.innerText = `${fullCups / totalCups * 100}%`
-    }
-
-   
 
 }}
 
