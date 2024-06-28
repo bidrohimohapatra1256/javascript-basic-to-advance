22e6094eb62977859be4cc97198453595f9d74d8
BIDROHI MOHAPATRA <118372536+bidrohimohapatra1256@users.noreply.github.com>
2024-06-28 22:02:16 +0530

    commit

--- landingpage2/main.js
+++ landingpage2/main.js
@@ -1,7 +1,7 @@
 
 function checkPalindrome() {
     const inputText = document.getElementById('inputText').value.toLowerCase().replace(/[^a-z0-9]/g, '');
-    const reversedText = inputText.split('').reverse().join('');
+
     
     
 c
